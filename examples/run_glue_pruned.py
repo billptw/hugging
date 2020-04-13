3b588afd62d3e9a73446ac2c0ae6d6a1271e8a7d
Pung Tuck Weng <pungtuckweng@Bill-MacBook.local>
2020-04-13 18:11:53 +0800

    db

--- examples/run_glue_pruned.py
+++ examples/run_glue_pruned.py
@@ -175,6 +175,7 @@ def train(args, train_dataset, model, tokenizer):
         print('Pruning Model...')
 
         for name, module in list(model.named_modules()):
+            print(name)
             if 'weight' in name:
                 print(name)
                 prune.random_unstructured(module, name="weight", amount=0.9)
