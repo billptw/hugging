5d732c7de6bccd3b7db1a0f2839850ede2436d9d
Pung Tuck Weng <pungtuckweng@Bill-MacBook.local>
2020-04-16 11:52:57 +0800

    db

--- examples/run_glue_pruned.py
+++ examples/run_glue_pruned.py
@@ -337,10 +337,12 @@ def evaluate(args, model, tokenizer, prefix=""):
             for name, value in list(module.named_parameters()):
                 if name in ['weight']:
                     print(mod_name, name)
+                    print('weights before', float(torch.sum(module.weight == 0)))
                     if prune.is_pruned(module): prune.remove(module, 'weight')
                     if args.prune == 'global': parameters_to_prune.append((module, name))
                     elif args.prune == 'l1': module = prune.l1_unstructured(module, name=name, amount=args.prune_eval)
                     elif args.prune == 'random': module = prune.random_unstructured(module, name=name, amount=args.prune_eval)
+                    print('weights pruned', float(torch.sum(module.weight == 0)))
         if args.prune == 'global': prune.global_unstructured(parameters_to_prune, pruning_method=prune.L1Unstructured, amount=args.prune_eval)
         print('embeddings before', float(torch.sum(model.bert.embeddings.word_embeddings.weight == 0)))
         prune.l1_unstructured(model.bert.embeddings.word_embeddings, 'weight', amount=args.prune_eval)
