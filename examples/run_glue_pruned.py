bb8ff8194efff6deeea0140cda6eeb015a7a46f7
Pung Tuck Weng <pungtuckweng@Bill-MacBook.local>
2020-04-16 12:24:54 +0800

    db

--- examples/run_glue_pruned.py
+++ examples/run_glue_pruned.py
@@ -211,8 +211,8 @@ def train(args, train_dataset, model, tokenizer):
                     print('weights before {:.3f}%'.format(float(torch.sum(module.weight == 0)) * 100 / float(module.weight.nelement())))
                     if prune.is_pruned(module): prune.remove(module, 'weight')
                     if args.prune == 'global': parameters_to_prune.append((module, 'weight'))
-                    elif args.prune == 'l1': prune.l1_unstructured(module, name='weight', amount=args.prune_train)
-                    elif args.prune == 'random': prune.random_unstructured(module, name='weight', amount=args.prune_train)
+                    elif args.prune == 'l1': module = prune.l1_unstructured(module, name='weight', amount=args.prune_train)
+                    elif args.prune == 'random': module = prune.random_unstructured(module, name='weight', amount=args.prune_train)
                     print('weights after {:.3f}%'.format(float(torch.sum(module.weight == 0)) * 100 / float(module.weight.nelement())))
         if args.prune == 'global': prune.global_unstructured(parameters_to_prune, pruning_method=prune.L1Unstructured, amount=args.prune_train)
 
